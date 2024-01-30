456278eaccbd3f723b592576c0bc4b35d3f8aebd
PeifanT <158132315+PeifanT@users.noreply.github.com>
2024-01-30 02:10:42 -0500

    Delete question1.py

--- question1.py
+++
@@ -1,8 +0,0 @@
-# Write a program that prints a mulSplicaSon table for numbers up to 12
-def multiplication_table():
-    for i in range(1, 13):
-        table = "\n".join([f"{i} x {j} = {i * j}" for j in range(1, 13)])
-        print(table)
-        print("\n")
-
-multiplication_table()
